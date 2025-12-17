1538f7d8960ef282a44a39f0f1e6002c9a1c49de
TANUJADEVIDADISETTY <147792227+TANUJADEV1DADISETTY@users.noreply.github.com>
2025-12-17 23:05:15 +0530

    Time: 2 ms (71.88%) | Memory: 45.3 MB (46.60%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int[] res = new int[prices.length];
+
+        for(int i = 0; i < prices.length; i++) {
+            res[i] = prices[i];
+            for(int j = i + 1; j < prices.length; j++) {
+                if(prices[i] >= prices[j]) {
+                    res[i] = (prices[i] - prices[j]);
+                    break;
+                }
+                
+            }
+        }
+        // res[res.length - 1] = prices[prices.length - 1];
+        return res;
+    }
+}
