ec526b7e17bf2f1a0aec617196b28490f8b17fa6
TANUJADEVIDADISETTY <147792227+TANUJADEV1DADISETTY@users.noreply.github.com>
2026-01-20 13:54:51 +0530

    Time: 27 ms (12.28%) | Memory: 46.8 MB (30.70%) - LeetSync

---
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int[] ans = new int[nums.size()];
+
+        for(int i = 0; i < nums.size(); i++) {
+            // int temp = nums.get(i);
+           for(int j = 1; j <= nums.get(i); j++) {
+                if((j | (j+1)) == nums.get(i)) {
+                    ans[i] = j;
+                    break;
+                }
+           }
+           if(ans[i] == 0) {
+            ans[i] = -1;
+           }
+
+        }
+        return ans; 
+    }
+}
