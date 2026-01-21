2ab55ea01939564286925d98970d893e3321af5c
TANUJADEVIDADISETTY <147792227+TANUJADEV1DADISETTY@users.noreply.github.com>
2026-01-21 20:46:34 +0530

    Time: 5 ms (98.60%) | Memory: 48 MB (15.71%) - LeetSync

---
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDistinct(String s) {
+        int[] freq = new int[26];
+
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            freq[ch - 'a']++;
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] != 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
