759814de298413e26296d71c49a2702e8a3eebe9
TANUJADEVIDADISETTY <147792227+TANUJADEV1DADISETTY@users.noreply.github.com>
2026-08-05 14:52:27 +0530

    Time: 4 ms (99.03%) | Memory: 47.8 MB (28.80%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public boolean isPalindrome(String s, int left, int right) {
+        int i = left;
+        int j = right;
+        while(i < j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while(i < j) {
+            if(s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            }
+            else {
+                return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+            }
+        }
+        return true;
+
+    }
+}
